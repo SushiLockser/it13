622a05b7bc8ef564461207272a021e579c16a867
Straña Alynne <salockser@gmail.com>
2023-11-22 01:47:01 +0800

    Edited Edit Product Page

--- IT13_Proj-2/IT13_Proj-2/EditProductPage.Designer.vb
+++ IT13_Proj-2/IT13_Proj-2/EditProductPage.Designer.vb
@@ -25,22 +25,33 @@ Partial Class EditProductPage
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(EditProductPage))
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.DomainUpDown1 = New System.Windows.Forms.DomainUpDown()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.DomainUpDown2 = New System.Windows.Forms.DomainUpDown()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.Button9 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Helvetica", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Location = New System.Drawing.Point(11, 72)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(242, 40)
         Me.Label2.TabIndex = 30
@@ -48,66 +59,28 @@ Partial Class EditProductPage
         '
         'Panel1
         '
-        Me.Panel1.Location = New System.Drawing.Point(468, 190)
+        Me.Panel1.Location = New System.Drawing.Point(570, 236)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(458, 430)
+        Me.Panel1.Size = New System.Drawing.Size(356, 349)
         Me.Panel1.TabIndex = 31
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(17, 277)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(253, 22)
-        Me.TextBox1.TabIndex = 32
-        Me.TextBox1.Text = "Enter Product Name"
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(17, 334)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(253, 22)
-        Me.TextBox2.TabIndex = 33
-        Me.TextBox2.Text = "Enter Quantity"
         '
         'DomainUpDown1
         '
-        Me.DomainUpDown1.Location = New System.Drawing.Point(18, 400)
+        Me.DomainUpDown1.Font = New System.Drawing.Font("Helvetica", 10.0!)
+        Me.DomainUpDown1.Location = New System.Drawing.Point(19, 340)
         Me.DomainUpDown1.Name = "DomainUpDown1"
-        Me.DomainUpDown1.Size = New System.Drawing.Size(252, 22)
+        Me.DomainUpDown1.Size = New System.Drawing.Size(252, 27)
         Me.DomainUpDown1.TabIndex = 34
         Me.DomainUpDown1.Text = "Select Product Type"
         '
-        'TextBox3
+        'TextBox2
         '
-        Me.TextBox3.Location = New System.Drawing.Point(17, 463)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(253, 22)
-        Me.TextBox3.TabIndex = 35
-        Me.TextBox3.Text = "Enter Price"
-        '
-        'TextBox4
-        '
-        Me.TextBox4.Location = New System.Drawing.Point(17, 531)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(253, 22)
-        Me.TextBox4.TabIndex = 36
-        Me.TextBox4.Text = "Enter Supplier"
-        '
-        'DomainUpDown2
-        '
-        Me.DomainUpDown2.Location = New System.Drawing.Point(19, 598)
-        Me.DomainUpDown2.Name = "DomainUpDown2"
-        Me.DomainUpDown2.Size = New System.Drawing.Size(252, 22)
-        Me.DomainUpDown2.TabIndex = 37
-        Me.DomainUpDown2.Text = "Quality"
-        '
-        'TextBox5
-        '
-        Me.TextBox5.Location = New System.Drawing.Point(19, 220)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(251, 22)
-        Me.TextBox5.TabIndex = 38
-        Me.TextBox5.Text = "ID: "
+        Me.TextBox2.Font = New System.Drawing.Font("Helvetica", 10.0!)
+        Me.TextBox2.Location = New System.Drawing.Point(18, 236)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(253, 27)
+        Me.TextBox2.TabIndex = 33
+        Me.TextBox2.Text = "25"
         '
         'Button9
         '
@@ -117,7 +90,7 @@ Partial Class EditProductPage
         Me.Button9.ForeColor = System.Drawing.Color.White
         Me.Button9.Image = Global.IT13_Proj_2.My.Resources.Resources.Add_box__1_
         Me.Button9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button9.Location = New System.Drawing.Point(891, 137)
+        Me.Button9.Location = New System.Drawing.Point(889, 188)
         Me.Button9.Name = "Button9"
         Me.Button9.Size = New System.Drawing.Size(34, 30)
         Me.Button9.TabIndex = 40
@@ -132,7 +105,7 @@ Partial Class EditProductPage
         Me.Button8.ForeColor = System.Drawing.Color.White
         Me.Button8.Image = Global.IT13_Proj_2.My.Resources.Resources.Add_box__1_
         Me.Button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button8.Location = New System.Drawing.Point(755, 136)
+        Me.Button8.Location = New System.Drawing.Point(753, 187)
         Me.Button8.Name = "Button8"
         Me.Button8.Size = New System.Drawing.Size(130, 30)
         Me.Button8.TabIndex = 39
@@ -140,20 +113,188 @@ Partial Class EditProductPage
         Me.Button8.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button8.UseVisualStyleBackColor = False
         '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(333, 126)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(28, 21)
+        Me.Label7.TabIndex = 27
+        Me.Label7.Text = "---"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(211, 126)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(113, 21)
+        Me.Label6.TabIndex = 26
+        Me.Label6.Text = "Updated at : "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(132, 126)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 21)
+        Me.Label5.TabIndex = 25
+        Me.Label5.Text = "---"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(18, 126)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(108, 21)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "Total Value: "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Helvetica", 10.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(19, 446)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(253, 27)
+        Me.TextBox1.TabIndex = 41
+        Me.TextBox1.Text = "Php 20.00"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Helvetica", 10.0!)
+        Me.TextBox3.Location = New System.Drawing.Point(19, 558)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(253, 27)
+        Me.TextBox3.TabIndex = 42
+        Me.TextBox3.Text = "Enter Supplier"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Helvetica", 10.0!)
+        Me.TextBox4.Location = New System.Drawing.Point(300, 236)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(253, 27)
+        Me.TextBox4.TabIndex = 43
+        Me.TextBox4.Text = "15"
+        '
+        'DomainUpDown2
+        '
+        Me.DomainUpDown2.Font = New System.Drawing.Font("Helvetica", 10.0!)
+        Me.DomainUpDown2.Location = New System.Drawing.Point(300, 340)
+        Me.DomainUpDown2.Name = "DomainUpDown2"
+        Me.DomainUpDown2.Size = New System.Drawing.Size(252, 27)
+        Me.DomainUpDown2.TabIndex = 44
+        Me.DomainUpDown2.Text = "Quality"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(19, 212)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 21)
+        Me.Label1.TabIndex = 45
+        Me.Label1.Text = "Quantity"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(19, 316)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 21)
+        Me.Label3.TabIndex = 46
+        Me.Label3.Text = "Product Type"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(19, 422)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(132, 21)
+        Me.Label8.TabIndex = 47
+        Me.Label8.Text = "Price per Piece"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(14, 534)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(76, 21)
+        Me.Label9.TabIndex = 48
+        Me.Label9.Text = "Supplier"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(296, 212)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(124, 21)
+        Me.Label10.TabIndex = 49
+        Me.Label10.Text = "Quantity Used"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(296, 316)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(66, 21)
+        Me.Label11.TabIndex = 50
+        Me.Label11.Text = "Quality"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Helvetica", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(22, 21)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(0, 21)
+        Me.Label12.TabIndex = 51
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Helvetica", 10.0!)
+        Me.Button1.Location = New System.Drawing.Point(19, 22)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 32)
+        Me.Button1.TabIndex = 52
+        Me.Button1.Text = "< back"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'EditProductPage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(969, 709)
-        Me.Controls.Add(Me.Button9)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.DomainUpDown2)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.DomainUpDown1)
-        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DomainUpDown1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label2)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -167,13 +308,24 @@ Partial Class EditProductPage
 
     Friend WithEvents Label2 As Label
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents DomainUpDown1 As DomainUpDown
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents DomainUpDown2 As DomainUpDown
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents Button9 As Button
-    Friend WithEvents Button8 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- IT13_Proj-2/IT13_Proj-2/EditProductPage.vb
+++ IT13_Proj-2/IT13_Proj-2/EditProductPage.vb
@@ -1,9 +1,2 @@
 ﻿Public Class EditProductPage
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub DomainUpDown1_SelectedItemChanged(sender As Object, e As EventArgs)
-
-    End Sub
 End Class
