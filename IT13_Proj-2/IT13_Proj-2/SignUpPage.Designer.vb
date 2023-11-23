14f95e80a19c06a564c0938c1377c7c09e7ca64f
Phoebe Lerog <p.lerog.524638@umindanao.edu.ph>
2023-11-23 11:56:13 +0800

    wow so wow

--- IT13_Proj-2/IT13_Proj-2/SignUpPage.Designer.vb
+++ IT13_Proj-2/IT13_Proj-2/SignUpPage.Designer.vb
@@ -55,65 +55,72 @@ Partial Class SignUpPage
         Me.Panel2.Controls.Add(Me.Label5)
         Me.Panel2.Controls.Add(Me.Label4)
         Me.Panel2.Controls.Add(Me.Label3)
-        Me.Panel2.Location = New System.Drawing.Point(725, 100)
+        Me.Panel2.Location = New System.Drawing.Point(544, 81)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(384, 589)
+        Me.Panel2.Size = New System.Drawing.Size(288, 479)
         Me.Panel2.TabIndex = 4
         '
         'TextBox4
         '
-        Me.TextBox4.Font = New System.Drawing.Font("Helvetica", 10.0!)
-        Me.TextBox4.Location = New System.Drawing.Point(40, 345)
+        Me.TextBox4.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.TextBox4.Location = New System.Drawing.Point(30, 280)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(304, 27)
+        Me.TextBox4.Size = New System.Drawing.Size(229, 23)
         Me.TextBox4.TabIndex = 12
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Helvetica", 10.0!)
-        Me.Label2.Location = New System.Drawing.Point(40, 322)
+        Me.Label2.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.Label2.Location = New System.Drawing.Point(30, 262)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.Size = New System.Drawing.Size(41, 16)
         Me.Label2.TabIndex = 11
         Me.Label2.Text = "Email"
         '
         'TextBox3
         '
-        Me.TextBox3.Font = New System.Drawing.Font("Helvetica", 10.0!)
-        Me.TextBox3.Location = New System.Drawing.Point(40, 419)
+        Me.TextBox3.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.TextBox3.Location = New System.Drawing.Point(30, 340)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(304, 27)
+        Me.TextBox3.Size = New System.Drawing.Size(229, 23)
         Me.TextBox3.TabIndex = 10
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Helvetica", 10.0!)
-        Me.Label1.Location = New System.Drawing.Point(40, 396)
+        Me.Label1.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.Label1.Location = New System.Drawing.Point(30, 322)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(83, 20)
+        Me.Label1.Size = New System.Drawing.Size(68, 16)
         Me.Label1.TabIndex = 9
         Me.Label1.Text = "Password"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Font = New System.Drawing.Font("Helvetica", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label8.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold)
         Me.Label8.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Label8.Location = New System.Drawing.Point(245, 545)
+        Me.Label8.Location = New System.Drawing.Point(184, 443)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(54, 17)
+        Me.Label8.Size = New System.Drawing.Size(41, 15)
         Me.Label8.TabIndex = 8
         Me.Label8.Text = "Log In"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Font = New System.Drawing.Font("Helvetica", 9.0!)
-        Me.Label7.Location = New System.Drawing.Point(67, 545)
+        Me.Label7.Font = New System.Drawing.Font("Arial", 9.0!)
+        Me.Label7.Location = New System.Drawing.Point(50, 443)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(178, 17)
+        Me.Label7.Size = New System.Drawing.Size(146, 15)
         Me.Label7.TabIndex = 7
         Me.Label7.Text = "Login your Admin Account"
         '
@@ -121,62 +128,68 @@ Partial Class SignUpPage
         '
         Me.Button1.BackColor = System.Drawing.SystemColors.Highlight
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button1.Font = New System.Drawing.Font("Helvetica", 10.2!, System.Drawing.FontStyle.Bold)
+        Me.Button1.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Bold)
         Me.Button1.ForeColor = System.Drawing.Color.Snow
-        Me.Button1.Location = New System.Drawing.Point(40, 481)
+        Me.Button1.Location = New System.Drawing.Point(30, 391)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(304, 40)
+        Me.Button1.Size = New System.Drawing.Size(228, 32)
         Me.Button1.TabIndex = 6
         Me.Button1.Text = "Create Account"
         Me.Button1.UseVisualStyleBackColor = False
         '
         'TextBox1
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Helvetica", 10.0!)
-        Me.TextBox1.Location = New System.Drawing.Point(40, 269)
+        Me.TextBox1.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(30, 219)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(304, 27)
+        Me.TextBox1.Size = New System.Drawing.Size(229, 23)
         Me.TextBox1.TabIndex = 3
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Helvetica", 10.0!)
-        Me.Label5.Location = New System.Drawing.Point(40, 246)
+        Me.Label5.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.Label5.Location = New System.Drawing.Point(30, 200)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(53, 20)
+        Me.Label5.Size = New System.Drawing.Size(43, 16)
         Me.Label5.TabIndex = 2
         Me.Label5.Text = "Name"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Helvetica", 10.0!)
-        Me.Label4.Location = New System.Drawing.Point(26, 161)
+        Me.Label4.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.Label4.Location = New System.Drawing.Point(20, 131)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(240, 20)
+        Me.Label4.Size = New System.Drawing.Size(199, 16)
         Me.Label4.TabIndex = 1
         Me.Label4.Text = "Just a few things to get started"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Helvetica", 32.0!, System.Drawing.FontStyle.Bold)
-        Me.Label3.Location = New System.Drawing.Point(19, 31)
+        Me.Label3.Font = New System.Drawing.Font("Arial", 32.0!, System.Drawing.FontStyle.Bold)
+        Me.Label3.Location = New System.Drawing.Point(14, 25)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(349, 130)
+        Me.Label3.Size = New System.Drawing.Size(275, 102)
         Me.Label3.TabIndex = 0
-        Me.Label3.Text = "Create Satff " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Account"
+        Me.Label3.Text = "Create Staff " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Account"
         '
         'Label9
         '
         Me.Label9.AutoSize = True
         Me.Label9.BackColor = System.Drawing.Color.Transparent
-        Me.Label9.Font = New System.Drawing.Font("Helvetica", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label9.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold)
         Me.Label9.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Label9.Location = New System.Drawing.Point(265, 22)
+        Me.Label9.Location = New System.Drawing.Point(199, 18)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(131, 17)
+        Me.Label9.Size = New System.Drawing.Size(101, 15)
         Me.Label9.TabIndex = 15
         Me.Label9.Text = "[Admin Account]"
         '
@@ -184,10 +197,11 @@ Partial Class SignUpPage
         '
         Me.Label10.AutoSize = True
         Me.Label10.BackColor = System.Drawing.Color.Transparent
-        Me.Label10.Font = New System.Drawing.Font("Helvetica", 9.0!, System.Drawing.FontStyle.Bold)
-        Me.Label10.Location = New System.Drawing.Point(58, 22)
+        Me.Label10.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label10.Location = New System.Drawing.Point(44, 18)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(201, 17)
+        Me.Label10.Size = New System.Drawing.Size(155, 15)
         Me.Label10.TabIndex = 14
         Me.Label10.Text = "GVAlias Architectural Firm"
         '
@@ -196,24 +210,26 @@ Partial Class SignUpPage
         Me.Panel1.BackColor = System.Drawing.Color.Transparent
         Me.Panel1.BackgroundImage = Global.IT13_Proj_2.My.Resources.Resources.Untitled_design__1_
         Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Location = New System.Drawing.Point(9, 10)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(40, 40)
+        Me.Panel1.Size = New System.Drawing.Size(30, 32)
         Me.Panel1.TabIndex = 13
         '
         'SignUpPage
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.IT13_Proj_2.My.Resources.Resources.Sign_Up_Staff
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1188, 756)
+        Me.ClientSize = New System.Drawing.Size(891, 614)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.Panel1)
         Me.DoubleBuffered = True
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "SignUpPage"
         Me.Text = "Sign Up"
         Me.Panel2.ResumeLayout(False)
